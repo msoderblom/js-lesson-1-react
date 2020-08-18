e7dfea2f9e1bcd9e554e917c20a94ca9bcb610f1
Matilda Söderblom <soderblom.matilda@gmail.com>
2020-08-18 15:31:16 +0200

    added btn component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import MyButton from "./components/MyButton";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>My react app</h1>
+
+        <p>This is my second react app</p>
+        <MyButton title="Tryck här"></MyButton>
+        <MyButton title="Read more"></MyButton>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/msoderblom"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          My GitHub
         </a>
       </header>
     </div>
---
+++ src/components/MyButton.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function MyButton({ title }) {
+  return <button className="btn btn-primary">{title}</button>;
+}
